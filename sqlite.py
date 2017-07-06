9e7e0be621ba45959538a9587eb2626f9a6c839e
Chris Zuber <shgysk8zer0@gmail.com>
2017-07-05 19:38:08 -0700

    Add easy `insert` & `select` methods

--- sqlite.py
+++ sqlite.py
@@ -8,3 +8,29 @@ class Sqlite(sqlite3.Connection):
 
     def prepare(self, sql: str) -> PreparedStatement:
         return PreparedStatement(self, sql)
+
+    def select(self, table: str, *args, **kwargs) -> sqlite3.Cursor:
+        if len(args) > 0:
+            cols = self.__format_cols(args)
+        else:
+            cols = ['*']
+
+        if len(kwargs) > 0:
+            bindings = self.__format_bindings(kwargs.keys())
+            sql = 'SELECT {} FROM `{}` WHERE {};'.format(', '.join(cols), table, ' AND '.join(bindings))
+        else:
+            sql = 'SELECT {} FROM `{}`;'.format(', '.join(cols), table)
+
+        return self.cursor().execute(sql, kwargs)
+
+    def insert(self, table, **kwargs):
+        cols = self.__format_cols(kwargs.keys())
+        bindings = self.__format_bindings(kwargs.keys())
+        sql = 'INSERT INTO `{}` ({}) VALUES ({});'.format(table, ', '.join(cols), ', '.join(bindings))
+        return self.execute(sql, kwargs)
+
+    def __format_cols(self, cols: tuple) -> list:
+        return ['`{}`'.format(key) for key in cols]
+
+    def __format_bindings(self, cols: tuple) -> list:
+        return [':{}'.format(key) for key in cols]
